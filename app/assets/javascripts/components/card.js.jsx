1487ace340fd01d7562dd12ebf0855935862c0f1
Leon Mok <mok.leon@gmail.com>
2016-03-30 00:12:01 +0800

    Fix typos in card.js.jsx

--- app/assets/javascripts/components/card.js.jsx
+++ app/assets/javascripts/components/card.js.jsx
@@ -1,21 +1,19 @@
-import React from 'react';
-
-export default class Card extends React.Component {
+class Card extends React.Component {
 
   constructor(props) {
     super(props);
 
-    let { card } = this.props;
-    cardName = card.name
-                .trim()
-                .replace(/[^a-zA-Z0-9-\s-\']/g, '')
-                .replace(/[^a-zA-Z0-9-]/g, '-')
-                .replace(/--/g, '-')
-                .toLowerCase();
+    let { name } = this.props.card;
+    let cardName = name.
+                 .trim()
+                 .replace(/[^a-zA-Z0-9-\s-\']/g, '')
+                 .replace(/[^a-zA-Z0-9-]/g, '-')
+                 .replace(/--/g, '-')
+                 .toLowerCase();
 
     cardName = cardName === 'si7-agent' && 'si-7-agent';
 
-    const imgSrc = `//s3.amazonaws.com/hearthstatsprod/full_card/${cn}.png`
+    const imgSrc = `//s3.amazonaws.com/hearthstatsprod/full_card/${cardName}.png`
 
     this.state = {
       hasLoaded: false,
@@ -41,7 +39,7 @@ export default class Card extends React.Component {
     const { hasLoaded, imgSrc } = this.state;
     const { cName } = this.props;
 
-    const className = `deckbuilder-img ${cname}`;
+    const className = `deckbuilder-img ${cName}`;
     const imageSource = hasLoaded ? imgSrc : '/assets/blind_draft/deckbuilder-card-back.png';
 
     return (
